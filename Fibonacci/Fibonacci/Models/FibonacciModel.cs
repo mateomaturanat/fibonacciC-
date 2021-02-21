898672331d7c894274a2c9f5882ee71e34eb0f6c
MateoMaturana <javier.380172789@ucaldas.edu.co>
2021-02-21 17:19:01 -0500

    Add fibonacci functionality

---
+++ Fibonacci/Fibonacci/Models/FibonacciModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Fibonacci.Models
+{
+    public class FibonacciModel
+    {
+        private int size;
+        private List<int> listFibonacci = new List<int>();
+
+        public FibonacciModel(int size, List<int> listFibonacci)
+        {
+            this.size = size;
+            this.listFibonacci = listFibonacci;
+        }
+        public FibonacciModel()
+        {
+        }
+        public int getSize()
+        {
+            return size;
+        }
+        public void setSize(int size)
+        {
+            this.size = size;
+        }
+        private void fibonacci(int num)
+        {
+            if (num > 1)
+            {
+                listFibonacci.Add(listFibonacci.ElementAt(num - 1) + listFibonacci.ElementAt(num - 2));
+            }
+            else if (num == 1)
+            {
+                listFibonacci.Add(1);
+            }
+            else if (num == 0)
+            {
+                listFibonacci.Add(0);
+            }
+            else
+            {
+                Console.WriteLine("El tamaño debe ser igual o superior a 1");
+            }
+        }
+        public void showFibonacci()
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                fibonacci(i);
+                Console.Write(this.listFibonacci.ElementAt(i) + ", ");
+            }
+        }
+    }
+}
--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fibonacci.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            FibonacciModel objFibonacciModel = new FibonacciModel();
+            objFibonacciModel.setSize(10);
+            objFibonacciModel.showFibonacci();
+            Console.ReadKey();
         }
     }
 }
